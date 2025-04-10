331c5508032d8b7477d1c7ed8c583f679a2791e2
Gabriel de Oliveira <gabriel.oliveira_012@hotmail.com>
2025-04-10 15:53:22 -0300

    update elements

--- cypress/e2e/elements.cy.js
+++ cypress/e2e/elements.cy.js
@@ -20,4 +20,25 @@ describe("Elementos", () => {
     cy.contains("Voltar").click();
     cy.get("#resultado").should("have.text", "Voltou!");
   });
+
+  it.only("TexfFields", () => {
+    cy.get("#formNome").type("teste");
+    cy.get("#formNome").should("have.value", "teste");
+
+    cy.get("#elementosForm\\:sugestoes")
+      .type("text area")
+      .should("have.value", "text area");
+
+    cy.get("#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6)")
+      .type("teste")
+    //   .should("have.value", "teste");
+
+    cy.get('[data-cy="dataSobrenome"]').type('teste12345{backspace}{backspace}')
+
+
+    cy.get("#elementosForm\\:sugestoes")
+    .clear()
+    .type('erro{selectall}acerto')
+    .should('have.value','acerto')
+  });
 });
